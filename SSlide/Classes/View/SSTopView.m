3a3ce52037392fa64bda7678dc8059719d3e84f2
Le Van Nghia <nghialv2607@gmail.com>
2013-08-21 17:04:24 +0900

    delete something

--- SSlide/Classes/View/SSTopView.m
+++ SSlide/Classes/View/SSTopView.m
@@ -21,10 +21,10 @@
 
 - (void) initView
 {
-    UIButton *loginBtn = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 100, 50)];
-    loginBtn.titleLabel.text = @"login";
-    loginBtn.titleLabel.textColor = [UIColor whiteColor];
-    loginBtn.backgroundColor = [UIColor greenSeaColor];
+//    UIButton *loginBtn = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 100, 50)];
+//    loginBtn.titleLabel.text = @"login";
+//    loginBtn.titleLabel.textColor = [UIColor whiteColor];
+//    loginBtn.backgroundColor = [UIColor greenSeaColor];
     [self addSubview:loginBtn];
 }
 
